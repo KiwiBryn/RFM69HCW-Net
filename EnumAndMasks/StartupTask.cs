c92984b6e501767de04269d5b9f8d87c10c3c7fc
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2019-07-13 18:46:51 +1200

    Initial refactoring pulling all the hardware setup into constructor(s) and register startup configuration into the Initialise method. Lost of parameters but most defaults and never touched...
    Used the RegIrqFlags enums in the interrupt handler method. Need to look at adding RSSI as well

--- EnumAndMasks/StartupTask.cs
+++ EnumAndMasks/StartupTask.cs
@@ -45,9 +45,9 @@ namespace devMobile.IoT.Rfm69Hcw.EnumAndMasks
 			RegBitrateLsb = 0x04,
 			RegFdevMsb = 0x05,
 			RegFdevLsb = 0x06,
-			RegFrMsb = 0x07,
-			RegFrMid = 0x08,
-			RegFrLsb = 0x09,
+			RegFrfMsb = 0x07,
+			RegFrfMid = 0x08,
+			RegFrfLsb = 0x09,
 			RegOsc1 = 0x0A,
 			RegAfcCtrl = 0x0B,
 			RegListen1 = 0x0D,
@@ -122,12 +122,217 @@ namespace devMobile.IoT.Rfm69Hcw.EnumAndMasks
 			MaxValue = RegAesKey16,
 		}
 
-		// RegOp Mode flags
+		// RegOpMode mode flags
+		private const byte RegOpModeSequencerOff = 0b10000000;
+		private const byte RegOpModeListenOn = 0b01000000;
+		private const byte RegOpModeListenOff = 0b00000000;
+		private const byte RegOpModeListenAbort = 0b01000000;
 
-		// RegVersion default expected valued
+		[Flags]
+		public enum RegOpModeMode : byte
+		{
+			Sleep = 0b00000000,
+			StandBy = 0b00000100,
+			FrequencySynthesisTx = 0b00001000,
+			Transmit = 0b00001100,
+			Receive = 0b00010000,
+		};
+
+		// BitRate configuration from table9 in datasheet RegBitrateMsb, RegBitrateLsb
+		public enum BitRate : ushort
+		{
+			bps1K2 = 0x682B,
+			bps2K4 = 0x3415,
+			bps4K8 = 0x1A0B,
+			bps9K6 = 0x0D05,
+			bps19K2 = 0x0683,
+			bps38K4 = 0x0341,
+			bps76K8 = 0x01A1,
+			bps153K6 = 0x00D0,
+			bps57K6 = 0x022Cc,
+			bps115K2 = 0x0116,
+			bps12K5 = 0x0A00,
+			bps25K = 0x0500,
+			bps100k = 0x0140,
+			bps150K = 0x00D5,
+			bps200K = 0x00A0,
+			bps250K = 0x0080,
+			bps300k = 0x006B
+		};
+		const BitRate BitRateDefault = BitRate.bps4K8;
+
+		// Frequency deviation configuration RegFdevMsb, RegFdevLsb 
+		const ushort frequencyDeviationDefault = 0x0052;
+
+		// Frequency configuration magic numbers from Semtech SX1231 specs RegFrMsb, RegFrMid, RegFrLsb
+		private const double RH_RF6M9HCW_FXOSC = 32000000.0;
+		private const double RH_RFM69HCW_FSTEP = RH_RF6M9HCW_FXOSC / 524288.0;
+		public const double FrequencyDefault = 915000000.0;
+
+		// RegListen1 settings
+		public enum ListenModeIdleResolution : byte
+		{
+			Reserved = 0b00000000,
+			IdleTime64us = 0b01000000,
+			IdleTime4_1ms = 0b10000000,
+			IdleTime262ms = 0b11000000,
+		}
+		const ListenModeIdleResolution ListenModeIdleResolutionDefault = ListenModeIdleResolution.IdleTime4_1ms;
+
+		public enum ListenModeRXTime : byte
+		{
+			Reserved = 0b00000000,
+			IdleTime64us = 0b00010000,
+			IdleTime4_1ms = 0b00100000,
+			IdleTime262ms = 0b00110000,
+		}
+		const ListenModeRXTime ListenModeRXTimeDefault = ListenModeRXTime.IdleTime64us;
+
+		public enum ListenModeCrieria : byte
+		{
+			RssiThreshold = 0b00000000,
+			RssiThresholdAndSyncAddressMatched = 0b00001000
+		}
+		const ListenModeCrieria ListenModeCrieriaDefault = ListenModeCrieria.RssiThreshold;
+
+		public enum ListenModeEnd : byte
+		{
+			StayInRXMode = 0b00000000,
+			StayInRxModeUntilPayloadReady = 0b00000010,
+			StayInRxModeUntilPayloadReadyOrTimeoutInterrupt = 0b00000100,
+			Reserved = 0b00000110,
+		}
+		const ListenModeEnd ListenModeEndDefault = ListenModeEnd.StayInRxModeUntilPayloadReadyOrTimeoutInterrupt;
+
+		// RegListen2 
+		const byte ListenCoefficientIdleDefault = 0xf5;
+
+		// RegListen3 
+		const byte ListenCoefficientReceiveDefault = 0x20;
+
+		// RegVersion default expected value
 		const byte RegVersionValueExpected = 0x24;
 
+		// RegPaLevel 
+		const bool pa0OnDefault = true;
+		const bool pa1OnDefaut = false;
+		const bool pa2OnDefault = false;
+		const byte OutputpowerDefault = 0b00011111;
+
+		// RegPaRamp 
+		public enum PaRamp : byte
+		{
+			Period3_4ms = 0b00000000,
+			Period2ms =   0b00000001,
+			Period1ms =   0b00000010,
+			Period500us = 0b00000011,
+			Period250us = 0b00000100,
+			Period125us = 0b00000101,
+			Period100us = 0b00000110,
+			Period62us =  0b00000111,
+			Period50us =  0b00001000,
+			Period40us =  0b00001001,
+			Period31us =  0b00001010,
+			Period25us =  0b00001011,
+			Period20us =  0b00001100,
+			Period15us =  0b00001101,
+			Period12us =  0b00001110,
+			Period10us =  0b00001111,
+		}
+		const PaRamp PaRampDefault = PaRamp.Period40us;
+
+		// RegOcp values
+		const bool OcpOnDefault = true;
+		const byte OcpTrimDefault = 0b0001010;
+
+		// RegLna
+		public enum LnaZin : byte
+		{
+			Impedance50Ohms = 0b00000000,
+			Impedance200Ohms = 0b10000000
+		}
+		const LnaZin LnaZinDefault = LnaZin.Impedance50Ohms;
+
+		public enum LnaCurrentGain : byte
+		{
+			Manual = 0b00100000,
+			Agc = 0b00000000,
+		}
+		const LnaCurrentGain LnaCurrentGainDefault = LnaCurrentGain.Manual;
+
+		public enum LnaGainSelect : byte
+		{
+			AGC = 0b00000000,
+			G1 = 0b00000001,
+			G2 = 0b00000010,
+			G3 = 0b00000011,
+			G4 = 0b00000100,
+			G5 = 0b00000101,
+			G6 = 0b00000110,
+			Reserved = 0b00000111,
+		} 
+		const LnaGainSelect LnaGainSelectDefault = LnaGainSelect.AGC;
+
+		// RegRxBw
+		const byte DccFrequencyDefault = 0b01000000;
+
+		public enum RxBwMant
+		{
+			RxBwMant16 = 0b00000000,
+			RxBwMant20 = 0b00001000,
+			RxBwMant24 = 0b00010000,
+			Reserved = 0b00011000
+		}
+		const RxBwMant RxBwMantDefault = RxBwMant.RxBwMant24;
+		const byte RxBwExpDefault = 0b00000101;
+
+		// RegAfcBW
+		const byte DccFreqAfcDefault = 0b10000000;
+		const byte RxBwMantAfcDefault = 0b00001000;
+		const byte RxBwExpAfcDefault = 0b00000011;
+
+		// Ignored these registers
+		// RegOokPeak, RegOokAvg, RegOokFix, RegAfcFei, 
+		// RegAfcMsb, RegAfcLsb
+		// RegFeiMsb, RegFeiLsb
+
+		// RegDioMapping1 & RegDioMapping1 CpntinuousMode Table 21/ Packet Mode Table 22 pg48
+
+		// RegIrqFlags1
+		[Flags]
+		public enum RegIrqFlags1 : byte
+		{
+			ModeReady = 0b10000000,
+			RxReady =   0b01000000,
+			TxReady =	0b00100000,
+			PllLock =	0b00010000,
+			Rssi =		0b00001000,
+			Timeout =	0b00000100,
+			AutoMode =  0b00000010,
+			SynAddressMatch = 0b00000001
+		}
+
+		// RegIrqFlags2
+		[Flags]
+		public enum RegIrqFlags2 : byte
+		{
+			FifoFull = 0b10000000,
+			FifoNotEmpty = 0b01000000,
+			FifoLevel = 0b00100000,
+			FifoOverrun = 0b00010000,
+			PacketSent = 0b00001000,
+			PayloadReady = 0b00000100,
+			CrcOk = 0b00000010,
+			// Unused = 0b00000001
+		}
+
+		// RegPreambleMsb
+		const ushort PreambleSizeDefault = 0x03;
+
+		// Hardware configuration support
+		private RegOpModeMode RegOpModeModeCurrent = RegOpModeMode.Sleep;
 		private GpioPin InterruptGpioPin = null;
+		private GpioPin ResetGpioPin = null;
 		public RegisterManager RegisterManager = null; // Future refactor this will be made private
 
 		public Rfm69HcwDevice(ChipSelectPin chipSelectPin, int resetPin, int interruptPin)
@@ -144,18 +349,192 @@ namespace devMobile.IoT.Rfm69Hcw.EnumAndMasks
 			GpioController gpioController = GpioController.GetDefault();
 
 			// Factory reset pin configuration
-			GpioPin resetGpioPin = gpioController.OpenPin(resetPin);
-			resetGpioPin.SetDriveMode(GpioPinDriveMode.Output);
-			resetGpioPin.Write(GpioPinValue.High);
-			Task.Delay(100);
-			resetGpioPin.Write(GpioPinValue.Low);
-			Task.Delay(10);
-
+			ResetGpioPin = gpioController.OpenPin(resetPin);
+			ResetGpioPin.SetDriveMode(GpioPinDriveMode.Output);
+	
 			// Interrupt pin for RX message & TX done notification 
 			InterruptGpioPin = gpioController.OpenPin(interruptPin);
-			resetGpioPin.SetDriveMode(GpioPinDriveMode.Input);
+			ResetGpioPin.SetDriveMode(GpioPinDriveMode.Input);
 
 			InterruptGpioPin.ValueChanged += InterruptGpioPin_ValueChanged;
+		}
+
+		public void RegisterDump()
+		{
+			RegisterManager.Dump((byte)Registers.MinValue, (byte)Registers.MaxValue);
+		}
+
+		
+		public void SetMode(RegOpModeMode mode)
+		{
+			byte regOpModeValue = (byte)mode;
+
+			RegisterManager.WriteByte((byte)Registers.RegOpMode, regOpModeValue);
+		}
+
+
+		public void Initialise(RegOpModeMode modeAfterInitialise,
+			BitRate bitRate = BitRateDefault,
+			ushort frequencyDeviation = frequencyDeviationDefault,
+			double frequency = FrequencyDefault,
+			ListenModeIdleResolution listenModeIdleResolution = ListenModeIdleResolutionDefault, ListenModeRXTime listenModeRXTime = ListenModeRXTimeDefault, ListenModeCrieria listenModeCrieria = ListenModeCrieriaDefault, ListenModeEnd listenModeEnd = ListenModeEndDefault,
+			byte listenCoefficientIdle = ListenCoefficientIdleDefault,
+			byte listenCoefficientReceive = ListenCoefficientReceiveDefault,
+			bool pa0On = pa0OnDefault, bool pa1On = pa1OnDefaut, bool pa2On = pa2OnDefault, byte outputpower = OutputpowerDefault,
+			PaRamp paRamp = PaRampDefault,
+			bool ocpOn = OcpOnDefault, byte ocpTrim = OcpTrimDefault,
+			LnaZin lnaZin = LnaZinDefault, LnaCurrentGain lnaCurrentGain = LnaCurrentGainDefault, LnaGainSelect lnaGainSelect = LnaGainSelectDefault,
+			byte dccFrequency = DccFrequencyDefault, RxBwMant rxBwMant = RxBwMantDefault, byte RxBwExp = RxBwExpDefault,
+			byte dccFreqAfc = DccFreqAfcDefault, byte rxBwMantAfc = RxBwMantAfcDefault, byte bxBwExpAfc = RxBwExpAfcDefault,
+			ushort preambleSize = PreambleSizeDefault
+			)
+		{
+			RegOpModeModeCurrent = modeAfterInitialise;
+
+			// Strobe Reset pin briefly to factory reset SX1231 chip
+			ResetGpioPin.Write(GpioPinValue.High);
+			Task.Delay(100);
+			ResetGpioPin.Write(GpioPinValue.Low);
+			Task.Delay(10);
+
+			// Put the device into sleep mode so registers can be changed
+			SetMode(RegOpModeMode.Sleep);
+			
+			// RegDataModul ignored
+
+			// RegBitrateMsb, RegBitrateLsb
+			if( bitRate != BitRateDefault)
+			{
+				byte[] bytes = BitConverter.GetBytes((ushort)bitRate);
+				RegisterManager.WriteByte((byte)Registers.RegBitrateMsb, bytes[1]);
+				RegisterManager.WriteByte((byte)Registers.RegBitrateLsb, bytes[0]);
+			}
+
+			// RegFdevMsb, RegFdevLsb
+			if( frequencyDeviation != frequencyDeviationDefault)
+			{
+				byte[] bytes = BitConverter.GetBytes((ushort)frequencyDeviation);
+				RegisterManager.WriteByte((byte)Registers.RegFdevMsb, bytes[1]);
+				RegisterManager.WriteByte((byte)Registers.RegFdevLsb, bytes[0]);
+			}
+
+			// Configure RF Carrier frequency RegFrMsb, RegFrMid, RegFrLsb
+			if (frequency != FrequencyDefault)
+			{
+				byte[] bytes = BitConverter.GetBytes((long)(frequency / RH_RFM69HCW_FSTEP));
+				RegisterManager.WriteByte((byte)Registers.RegFrfMsb, bytes[2]);
+				RegisterManager.WriteByte((byte)Registers.RegFrfMid, bytes[1]);
+				RegisterManager.WriteByte((byte)Registers.RegFrfLsb, bytes[0]);
+			}
+
+			if ((listenModeIdleResolution != ListenModeIdleResolutionDefault) ||
+				 (listenModeRXTime != ListenModeRXTimeDefault) ||
+				 (listenModeCrieria != ListenModeCrieriaDefault ) ||
+				 (listenModeEnd != ListenModeEndDefault))
+			{
+				byte regListen1Value = (byte)listenModeIdleResolution;
+
+				regListen1Value |= (byte)listenModeRXTime;
+				regListen1Value |= (byte)listenModeCrieria;
+				regListen1Value |= (byte)listenModeEnd;
+
+				RegisterManager.WriteByte((byte)Registers.RegListen1, regListen1Value);
+			}
+
+			if ( listenCoefficientIdle != ListenCoefficientIdleDefault)
+			{
+				RegisterManager.WriteByte((byte)Registers.RegListen2, listenCoefficientIdle);
+			}
+
+			if ( listenCoefficientReceive != ListenCoefficientReceiveDefault)
+			{
+				RegisterManager.WriteByte((byte)Registers.RegListen3, listenCoefficientReceive);
+			}
+
+			if ((pa0On != pa0OnDefault) || 
+				 (pa1On != pa1OnDefaut) ||  
+				 (pa2On != pa2OnDefault) || 
+				 (outputpower != OutputpowerDefault))
+			{
+				byte regPaLevelValue = outputpower;
+
+				if (pa0On)
+				{
+					regPaLevelValue |= 0b10000000;
+				}
+				if (pa1On)
+				{
+					regPaLevelValue |= 0b01000000;
+				}
+				if (pa2On)
+				{
+					regPaLevelValue |= 0b00100000;
+				}
+				RegisterManager.WriteByte((byte)Registers.RegPaLevel, regPaLevelValue);
+			}
+
+			// Set RegOcp if any of the settings not defaults
+			if ((ocpOn != OcpOnDefault) || 
+				 (ocpTrim != OcpTrimDefault))
+			{
+				byte regOcpValue = ocpTrim;
+
+				if (ocpOn)
+				{
+					regOcpValue |= 0b00010000;
+				}
+				RegisterManager.WriteByte((byte)Registers.RegOcp, regOcpValue);
+			}
+
+			// regLnaValue
+			if ((lnaZin != LnaZinDefault) ||
+				 (lnaCurrentGain != LnaCurrentGainDefault) ||
+				 (lnaGainSelect != LnaGainSelectDefault))
+			{
+				byte regLnaValue = (byte)lnaZin;
+
+				regLnaValue |= (byte)lnaCurrentGain;
+				regLnaValue |= (byte)lnaGainSelect;
+
+				RegisterManager.WriteByte((byte)Registers.RegLna, regLnaValue);
+			}
+
+			// RegRxBw
+			if ((dccFrequency != DccFrequencyDefault) ||
+				 (rxBwMant != RxBwMantDefault) ||
+				 (RxBwExp != RxBwExpDefault))
+			{
+				byte RegRxBwValue = (byte)DccFrequencyDefault;
+
+				RegRxBwValue |= (byte)RxBwMantDefault;
+				RegRxBwValue |= RxBwExpDefault;
+
+				RegisterManager.WriteByte((byte)Registers.RegRxBw, RegRxBwValue);
+			}
+
+			// RegAfcBw
+			if (( dccFreqAfc != DccFreqAfcDefault ) ||
+				 ( rxBwMantAfc != RxBwMantAfcDefault ) || 
+				 ( bxBwExpAfc != RxBwExpAfcDefault))
+			{
+				byte regAfcBwValue = dccFreqAfc;
+
+				regAfcBwValue |= rxBwMantAfc;
+				regAfcBwValue |= bxBwExpAfc;
+
+				RegisterManager.WriteByte((byte)Registers.RegAfcBw, regAfcBwValue);
+			}
+
+			// RegPreambleMsb RegPreambleLsb
+			if (preambleSize != PreambleSizeDefault)
+			{
+				byte[] bytes = BitConverter.GetBytes((ushort)preambleSize);
+				RegisterManager.WriteByte((byte)Registers.RegPreambleMsb, bytes[1]);
+				RegisterManager.WriteByte((byte)Registers.RegPreambleLsb, bytes[0]);
+			}
+
+			// Configure RegOpMode before returning
+			SetMode(modeAfterInitialise);
 		}
 
 		private void InterruptGpioPin_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
@@ -165,9 +544,9 @@ namespace devMobile.IoT.Rfm69Hcw.EnumAndMasks
 				return;
 			}
 
-			byte irqFlags = RegisterManager.ReadByte(0x28); // RegIrqFlags2
+			RegIrqFlags2 irqFlags = (RegIrqFlags2)RegisterManager.ReadByte((byte)Registers.RegIrqFlags2); // RegIrqFlags2
 			Debug.WriteLine("{0:HH:mm:ss.fff} RegIrqFlags {1}", DateTime.Now, Convert.ToString((byte)irqFlags, 2).PadLeft(8, '0'));
-			if ((irqFlags & 0b00000100) == 0b00000100)  // PayLoadReady set
+			if ((irqFlags & RegIrqFlags2.PayloadReady) == RegIrqFlags2.PayloadReady)  // PayLoadReady set
 			{
 				// Read the length of the buffer
 				byte numberOfBytes = RegisterManager.ReadByte(0x0);
@@ -184,16 +563,11 @@ namespace devMobile.IoT.Rfm69Hcw.EnumAndMasks
 				Debug.WriteLine("{0:HH:mm:ss} Received {1} byte message {2}", DateTime.Now, messageBytes.Length, messageText);
 			}
 
-			if ((irqFlags & 0b00001000) == 0b00001000)  // PacketSent set
+			if ((irqFlags & RegIrqFlags2.PacketSent) == RegIrqFlags2.PacketSent)  // PacketSent set
 			{
 				RegisterManager.WriteByte(0x01, 0b00010000); // RegOpMode set ReceiveMode
 				Debug.WriteLine("{0:HH:mm:ss.fff} Transmit-Done", DateTime.Now);
 			}
-		}
-
-		public void RegisterDump()
-		{
-			RegisterManager.Dump((byte)Registers.MinValue, (byte)Registers.MaxValue);
 		}
 	}
 
@@ -204,40 +578,23 @@ namespace devMobile.IoT.Rfm69Hcw.EnumAndMasks
 		private const int InterruptPin = 22;
 		private Rfm69HcwDevice rfm69Device = new Rfm69HcwDevice(ChipSelectPin.CS1, ResetPin, InterruptPin);
 
-		const double RH_RF6M9HCW_FXOSC = 32000000.0;
-		const double RH_RFM69HCW_FSTEP = RH_RF6M9HCW_FXOSC / 524288.0;
-
 		public void Run(IBackgroundTaskInstance taskInstance)
 		{
 			rfm69Device.RegisterDump();
 
-			// regOpMode standby
-			rfm69Device.RegisterManager.WriteByte(0x01, 0b00000100);
+			rfm69Device.Initialise( Rfm69HcwDevice.RegOpModeMode.StandBy,
+											bitRate:Rfm69HcwDevice.BitRate.bps4K8,
+											frequency:915000000.0, frequencyDeviation:0X023d,
+											preambleSize:16
+											);
 
-			// BitRate MSB/LSB
-			rfm69Device.RegisterManager.WriteByte(0x03, 0x34);
-			rfm69Device.RegisterManager.WriteByte(0x04, 0x00);
-
-			// Frequency deviation
-			rfm69Device.RegisterManager.WriteByte(0x05, 0x02);
-			rfm69Device.RegisterManager.WriteByte(0x06, 0x3d);
-
-			// Calculate the frequency accoring to the datasheett
-			byte[] bytes = BitConverter.GetBytes((uint)(915000000.0 / RH_RFM69HCW_FSTEP));
-			Debug.WriteLine("Byte Hex 0x{0:x2} 0x{1:x2} 0x{2:x2} 0x{3:x2}", bytes[0], bytes[1], bytes[2], bytes[3]);
-			rfm69Device.RegisterManager.WriteByte(0x07, bytes[2]);
-			rfm69Device.RegisterManager.WriteByte(0x08, bytes[1]);
-			rfm69Device.RegisterManager.WriteByte(0x09, bytes[0]);
+			rfm69Device.RegisterDump();
 
 			// RegRxBW
 			rfm69Device.RegisterManager.WriteByte(0x19, 0x2a);
 
 			// RegDioMapping1
 			rfm69Device.RegisterManager.WriteByte(0x26, 0x01);
-
-			// Setup preamble length to 16 (default is 3) RegPreambleMsb RegPreambleLsb
-			rfm69Device.RegisterManager.WriteByte(0x2C, 0x0);
-			rfm69Device.RegisterManager.WriteByte(0x2D, 0x10);
 
 			// RegSyncConfig Set the Sync length and byte values SyncOn + 3 custom sync bytes
 			rfm69Device.RegisterManager.WriteByte(0x2e, 0x90);
